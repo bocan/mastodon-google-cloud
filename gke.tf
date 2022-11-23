d623f71b4b21d347354a824eada7754fd6ab14c9
Chris Funderburg <chris@pitchup.com>
2022-11-23 22:47:57 +0000

    bump gke module to fix location-policy permadrifting

--- gke.tf
+++ gke.tf
@@ -1,6 +1,6 @@
 module "gke" {
   source                            = "terraform-google-modules/kubernetes-engine/google//modules/beta-private-cluster"
-  version                           = "23.3.0"
+  version                           = "24.0.0"
   project_id                        = var.project_id
   name                              = "gke-mastodon"
   region                            = var.region
